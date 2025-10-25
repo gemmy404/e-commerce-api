b39fdcdbce7808a9ef1433dc29ef4cfaf8c8c1ef
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2025-10-26 00:58:52 +0300

    add uploadFile multer config

---
+++ src/common/utils/multer-storage.ts
@@ -0,0 +1,26 @@
+import multer from "multer";
+import fs from "node:fs";
+
+export const createMulterStorage = () => {
+    return multer.diskStorage({
+        destination: (req, file, cb) => {
+            let uploadPath = './uploads';
+
+            if (req.url.includes('users')) {
+                uploadPath += `/profiles`;
+            } else if (req.url.includes('products')) {
+                uploadPath += `/products/${req.params.id}`;
+            }
+
+            if (!fs.existsSync(uploadPath)) {
+                fs.mkdirSync(uploadPath, {recursive: true});
+            }
+            cb(null, uploadPath);
+        },
+        filename: (req, file, cb) => {
+            const ext = file.mimetype.split('/')[1];
+            const fileName = `${Date.now()}.${ext}`;
+            cb(null, fileName);
+        }
+    })
+}
