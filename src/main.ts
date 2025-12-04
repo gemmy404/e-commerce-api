af4d9a0fe5ac94c644deb6f066dce9b38b22b6a4
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2025-12-04 22:51:58 +0200

    add rawBody support and RawBody decorator for accessing raw request payload
    - Enabled rawBody parsing in NestFactory configuration
    - Extended Express Request interface to include rawBody buffer
    - Added custom RawBody parameter decorator to access raw request body in controllers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import * as express from 'express';
+import {join} from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule, {
+        rawBody: true,
+        bodyParser: true,
+    });
+    app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+    app.use(express.static("templates"));
+    await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
